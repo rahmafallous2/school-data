1be712ba8135591e0d3c24222121389d4f41ffd5
rahmafallous2 <rahmifallous@gmail.com>
2025-06-24 16:21:47 +0300

    the most consistent student performance of student

--- school-data.py
+++ school-data.py
@@ -22,7 +22,27 @@ def highest_average():
     return highest_avg_name
 print(f"THe student with highest average is {highest_average()} ")
 
+#most consistent performance
+def most_consistent_performance():
+    most_consistent = 0
+    most_consistent_name = ""
+    for key in class_journal:
+        grades = class_journal[key]
 
+        highest = grades[0]
+        lowest = grades[0]
+        for grade in grades:
+            if grade > highest:
+                highest = grade
+            if grade < lowest:
+                lowest = grade
+    diff = highest - lowest
+    if smallest_diff is None or diff < smallest_diff:
+            smallest_diff = diff
+            most_consistent_name = key
+
+    return most_consistent_name, smallest_diff
+print(f"The most consistent performance's student is {most_consistent_performance()} ")
 
 #studens below 70
 def students_below_70():
